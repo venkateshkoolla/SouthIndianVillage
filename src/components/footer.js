0752d31e836003d3410303c0873af2fe529186f1
vishwa.koolla <koolla@ocas.ca>
2020-03-30 13:35:13 -0400

    Added footer

---
+++ src/components/footer.js
@@ -0,0 +1,35 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Footer = ({ siteTitle }) => (
+  <footer  style={{background: `#030303`, marginBottom: `1.45rem`, color: `#f5ce42`}}>
+    <div style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem`}}>
+    <div float = "left" style={{ margin: `0 auto`, maxWidth: 400, borderBottom: `1px solid`}}>
+    <h1 style={{ margin: 0 }}>
+        <Link to="/" style={{ fontFamily: `myFirstFont`, fontSize: `50px`, color: `#f5ce42`, textDecoration: `none`,textAlign: `center`
+                 }}>
+          {siteTitle}
+        </Link>
+      </h1>
+      <div Align="left">Tiffin services</div>
+    </div>
+    <div float = "left" style={{ margin: `0 auto`, maxWidth: 400}}>
+    <div id = "phone" style = {{float:`left`}} >647-862-5701</div>
+    <div id = "phone" style = {{float:`right`}}>southindianvillage@gmail.com</div>
+
+    </div>
+      
+    </div>
+  </footer>
+)
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Footer
